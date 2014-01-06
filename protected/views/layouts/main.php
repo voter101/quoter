fa94188ffd9f7f6cfe2ed478fcf883e2b5f3ecdc
Wiktor Mociun <voter101@gmail.com>
2014-01-06 17:21:20 +0100

    Basic template HTML

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,59 +1,34 @@
-<?php /* @var $this Controller */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
-
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css"/>
+    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
-
 <body>
-
-<div class="container" id="page">
-
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
-
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
-	<?php echo $content; ?>
-
-	<div class="clear"></div>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
-</div><!-- page -->
-
+    <div id="wrapper">
+        <header>
+            <?php echo CHtml::link("<h1>". Yii::app()->name ."</h1>", Yii::app()->request->baseUrl); ?>
+        </header>
+        <nav>
+            <?php
+            $this->widget('zii.widgets.CMenu', array(
+               'items' => array(
+                    array('label' => Yii::t("menu.add", "Add entry"), 'url' => array('/entry/add')),
+                    array('label' => Yii::t("menu.new", "New entries"), 'url' => array('/entry/new')),
+                    array('label' => Yii::t("menu.top", "Top entries"), 'url' => array('/entry/top')),
+                    array('label' => Yii::t("menu.bottom", "Worst entries"), 'url' => array('/entry/bottom')),
+                    array('label' => Yii::t("menu.archive", "Old entries"), 'url' => array('/entry/archive')),
+               ),
+            ));
+            ?>
+        </nav>
+	    <section id="content">
+		    <?php echo $content ?>
+	    </section>
+	    <footer>
+		    <span class="copyright">Copyright &copy; <?php echo date('Y'); ?></span>
+	    </footer>
+    </div>
 </body>
 </html>
