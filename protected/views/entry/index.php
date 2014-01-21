783c4ad27f1f8db1d35056abe993accf58a2a48f
Wiktor Mociun <voter101@gmail.com>
2014-01-21 11:02:48 +0100

    Entry view cleaning

--- protected/views/entry/index.php
+++ protected/views/entry/index.php
@@ -2,25 +2,7 @@
 /* @var $this EntryController */
 /* @var $dataProvider CActiveDataProvider */
 
-$this->breadcrumbs = array(
-	'Entries',
-);
-
-$this->menu = array(
-	array(
-		'label' => 'Create Entry',
-		'url' => array('create')
-	),
-	array(
-		'label' => 'Manage Entry',
-		'url' => array('admin')
-	),
-);
-?>
-
-<h1>Entries</h1>
-
-<?php $this->widget('zii.widgets.CListView', array(
+$this->widget('zii.widgets.CListView', array(
 	'dataProvider' => $dataProvider,
 	'itemView' => '_view',
 )); ?>
