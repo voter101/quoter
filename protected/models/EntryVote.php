f37958d362f0797e32cc9ea615bd65c1cba09e79
Wiktor Mociun <voter101@gmail.com>
2014-01-30 10:57:38 +0100

    Code reformat

--- protected/models/EntryVote.php
+++ protected/models/EntryVote.php
@@ -11,50 +11,62 @@
  * The followings are the available model relations:
  * @property Entry $id0
  */
-class EntryVote extends CActiveRecord
-{
+class EntryVote extends CActiveRecord {
 	/**
 	 * @return string the associated database table name
 	 */
-	public function tableName()
-	{
+	public function tableName() {
 		return 'entry_vote';
 	}
 
 	/**
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
+	public function rules() {
 		return array(
-			array('id, ip, possitive', 'required'),
-			array('id, possitive', 'numerical', 'integerOnly'=>true),
-			array('ip', 'length', 'max'=>48),
+			array(
+				'id, ip, possitive',
+				'required'
+			),
+			array(
+				'id, possitive',
+				'numerical',
+				'integerOnly' => true
+			),
+			array(
+				'ip',
+				'length',
+				'max' => 48
+			),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, ip, possitive', 'safe', 'on'=>'search'),
+			array(
+				'id, ip, possitive',
+				'safe',
+				'on' => 'search'
+			),
 		);
 	}
 
 	/**
 	 * @return array relational rules.
 	 */
-	public function relations()
-	{
+	public function relations() {
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'id0' => array(self::BELONGS_TO, 'Entry', 'id'),
+			'id0' => array(
+				self::BELONGS_TO,
+				'Entry',
+				'id'
+			),
 		);
 	}
 
 	/**
 	 * @return array customized attribute labels (name=>label)
 	 */
-	public function attributeLabels()
-	{
+	public function attributeLabels() {
 		return array(
 			'id' => 'ID',
 			'ip' => 'Ip',
@@ -74,18 +86,17 @@ class EntryVote extends CActiveRecord
 	 * @return CActiveDataProvider the data provider that can return the models
 	 * based on the search/filter conditions.
 	 */
-	public function search()
-	{
+	public function search() {
 		// @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria=new CDbCriteria;
+		$criteria = new CDbCriteria;
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('ip',$this->ip,true);
-		$criteria->compare('possitive',$this->possitive);
+		$criteria->compare('id', $this->id);
+		$criteria->compare('ip', $this->ip, true);
+		$criteria->compare('possitive', $this->possitive);
 
 		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
+			'criteria' => $criteria,
 		));
 	}
 
@@ -95,8 +106,7 @@ class EntryVote extends CActiveRecord
 	 * @param string $className active record class name.
 	 * @return EntryVote the static model class
 	 */
-	public static function model($className=__CLASS__)
-	{
+	public static function model($className = __CLASS__) {
 		return parent::model($className);
 	}
 }
