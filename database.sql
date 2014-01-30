8b6de63e9aa5b6531a0fac1948b75306dd1db368
Wiktor Mociun <voter101@gmail.com>
2014-01-30 10:29:00 +0100

    DB structure update

--- database.sql
+++ database.sql
@@ -1,30 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.0.8
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jan 21, 2014 at 11:12 AM
--- Server version: 5.5.34-MariaDB-log
--- PHP Version: 5.5.8
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `quoter`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `entry`
---
 
 CREATE TABLE IF NOT EXISTS `entry` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -37,27 +18,16 @@ CREATE TABLE IF NOT EXISTS `entry` (
   `type` smallint(6) NOT NULL DEFAULT '0',
   `deleted` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_vote`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
 
 CREATE TABLE IF NOT EXISTS `user_vote` (
   `id` int(11) NOT NULL,
   `ip` varchar(48) COLLATE utf8_bin NOT NULL,
+  `possitive` tinyint(1) NOT NULL,
   KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `user_vote`
---
 ALTER TABLE `user_vote`
   ADD CONSTRAINT `entry_id` FOREIGN KEY (`id`) REFERENCES `entry` (`id`) ON DELETE CASCADE;
 
