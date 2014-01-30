9960641f0c2fd046705d8308205b9ea04ae27339
Wiktor Mociun <voter101@gmail.com>
2014-01-30 10:55:11 +0100

    ACL

--- protected/controllers/EntryVoteController.php
+++ protected/controllers/EntryVoteController.php
@@ -1,46 +1,50 @@
 <?php
 
-class EntryVoteController extends Controller
-{
-	public function actionDelete()
-	{
+class EntryVoteController extends Controller {
+
+	public function filters() {
+		return array(
+			'accessControl',
+			// perform access control for CRUD operations
+			'postOnly + delete',
+			// we only allow deletion via POST request
+		);
+	}
+
+	public function accessRules() {
+		return array(
+			array(
+				'allow',
+				'actions' => array(
+					'voteUp',
+					'voteDown',
+				),
+				'users' => array('*'),
+			),
+			array(
+				'allow',
+				'actions' => array(
+					'delete'
+				),
+				'users' => array('admin'),
+			),
+			array(
+				'deny',
+				'users' => array('*'),
+			),
+		);
+	}
+
+	public function actionDelete() {
 		$this->render('delete');
 	}
 
-	public function actionVoteDown()
-	{
+	public function actionVoteDown() {
 		$this->render('voteDown');
 	}
 
-	public function actionVoteUp()
-	{
+	public function actionVoteUp() {
 		$this->render('voteUp');
 	}
 
-	// Uncomment the following methods and override them if needed
-	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
 }
