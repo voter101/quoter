bbda884e76ae887adfea4b21866b87fab8f84193
Wiktor Mociun <voter101@gmail.com>
2014-02-06 11:05:40 +0100

    Little clean-up

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -20,16 +20,6 @@ class SiteController extends Controller {
 	}
 
 	/**
-	 * This is the default 'index' action that is invoked
-	 * when an action is not explicitly requested by users.
-	 */
-	public function actionIndex() {
-		// renders the view file 'protected/views/site/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
-	}
-
-	/**
 	 * This is the action to handle external exceptions.
 	 */
 	public function actionError() {
