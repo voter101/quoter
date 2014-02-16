0ee7bbaf98e5d211599c2009c630bd8d38a44bd3
Wiktor Mociun <voter101@gmail.com>
2014-02-16 12:18:36 +0100

    Form builder skeleton

---
+++ protected/components/FormBuilder.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Make forms with less code
+ */
+class FormBuilder {
+
+	/**
+	 * @param       $model
+	 * @param array $inputs array of inputs. Input format specified on self::getField()
+	 * @param bool  $ajaxValidation
+	 * @param null  $submitText Default: text from internationalization library
+	 */
+	public static function BuildForm($model, array $inputs, $ajaxValidation = true, $submitText = null) {
+
+	}
+
+	public static function GetInput($model, $attribute, array $labelOptions, array $fieldOptions = null, array $errorOptions = null) {
+
+	}
+
+} 
