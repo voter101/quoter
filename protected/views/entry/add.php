c46f43e42b0e13d34534600ee17bc30468e05ff3
Wiktor Mociun <voter101@gmail.com>
2014-02-20 00:18:34 +0100

    Clean-up

--- protected/views/entry/add.php
+++ protected/views/entry/add.php
@@ -6,23 +6,4 @@
 /* @var $model Entry */
 /* @var $this EntryController */
 
-$this->breadcrumbs = array(
-	'Entries' => array('index'),
-	'Create',
-);
-
-$this->menu = array(
-	array(
-		'label' => 'List Entry',
-		'url' => array('index')
-	),
-	array(
-		'label' => 'Manage Entry',
-		'url' => array('admin')
-	),
-);
-?>
-
-<h1>Create Entry</h1>
-
-<?php $this->renderPartial('_form', array('model' => $model, 'formType' => 'add')); ?>
+$this->renderPartial('_form', array('model' => $model, 'formType' => 'add'));
--- protected/views/entry/create.php
+++ protected/views/entry/create.php
@@ -6,23 +6,4 @@
 /* @var $this EntryController */
 /* @var $model Entry */
 
-$this->breadcrumbs = array(
-	'Entries' => array('index'),
-	'Create',
-);
-
-$this->menu = array(
-	array(
-		'label' => 'List Entry',
-		'url' => array('index')
-	),
-	array(
-		'label' => 'Manage Entry',
-		'url' => array('admin')
-	),
-);
-?>
-
-	<h1>Create Entry</h1>
-
-<?php $this->renderPartial('_form', array('model' => $model, 'formType' => 'create')); ?>
+$this->renderPartial('_form', array('model' => $model, 'formType' => 'create'));
