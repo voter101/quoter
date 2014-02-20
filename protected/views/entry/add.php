a13adb987c94669ebf7e39393e517c91717a5595
Wiktor Mociun <voter101@gmail.com>
2014-02-20 02:23:16 +0100

    Forgot the echo

--- protected/views/entry/add.php
+++ protected/views/entry/add.php
@@ -6,6 +6,6 @@
 /* @var $model Entry */
 /* @var $this EntryController */
 ?>
-<h1><?php Yii::t('Entry.addForm.header', 'Add an entry'); ?></h1>
+<h1><?php echo Yii::t('Entry.addForm.header', 'Add an entry'); ?></h1>
 <?php
 $this->renderPartial('_form', array('model' => $model, 'formType' => 'add'));
--- protected/views/entry/create.php
+++ protected/views/entry/create.php
@@ -6,6 +6,6 @@
 /* @var $this EntryController */
 /* @var $model Entry */
 ?>
-<h1><?php Yii::t('Entry.addForm.header', 'Add an entry'); ?></h1>
+<h1><?php echo Yii::t('Entry.addForm.header', 'Add an entry'); ?></h1>
 <?php
 $this->renderPartial('_form', array('model' => $model, 'formType' => 'create'));
