0c38197029db51fa3f579588a0cfa72a1d87ebc9
Wiktor Mociun <voter101@gmail.com>
2014-04-09 17:20:04 +0200

    User fixtures

--- protected/tests/fixtures/User.php
+++ protected/tests/fixtures/User.php
@@ -1,7 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: voter101
- * Date: 09.04.14
- * Time: 14:28
- */ 
+return [
+	'user1' => [
+		'id' => 1,
+	    'email' => 'example@example.com',
+	    'password' => CPasswordHelper::hashPassword($salt = CPasswordHelper::generateSalt() . "trololo"),
+	    'salt' => $salt,
+	    'token' => md5(microtime())
+	],
+];
