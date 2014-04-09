21ecba10a31750540232ffdf5cf6e1e79208a8f2
Wiktor Mociun <voter101@gmail.com>
2014-04-09 17:18:36 +0200

    Remove test config

---
+++ protected/tests/fixtures/User.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: voter101
+ * Date: 09.04.14
+ * Time: 14:28
+ */ 
---
+++ protected/tests/unit/UserRepositoryTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: voter101
+ * Date: 09.04.14
+ * Time: 14:48
+ */
+
+namespace unit;
+
+
+class UserRepositoryTest {
+
+} 
