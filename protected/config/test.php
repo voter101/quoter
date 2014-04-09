310e9ada74dff9a659b660a03210e9581cbb3525
Wiktor Mociun <voter101@gmail.com>
2014-04-09 14:19:33 +0200

    Tests DB

--- protected/config/test.php
+++ protected/config/test.php
@@ -1,14 +1,12 @@
 <?php
-
+require_once "database.php";
 return CMap::mergeArray(require(dirname(__FILE__) . '/main.php'), array(
 		'components' => array(
 			'fixture' => array(
 				'class' => 'system.test.CDbFixtureManager',
 			),
 			/* uncomment the following to provide test database connection
-			'db'=>array(
-				'connectionString'=>'DSN for test database',
-			),
+			'db'=> $dbTest,
 			*/
 		),
 	));
