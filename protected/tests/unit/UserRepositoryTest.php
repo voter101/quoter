defb92dde44dcae7ba1930d62006bf3e236bb4ec
Wiktor Mociun <voter101@gmail.com>
2014-04-09 19:31:27 +0200

    More tests. ;)

--- protected/tests/unit/UserRepositoryTest.php
+++ protected/tests/unit/UserRepositoryTest.php
@@ -2,6 +2,7 @@
 
 Yii::import('application.models.User');
 Yii::import('application.repositories.UserRepository');
+Yii::import('application.exceptions.UserSaveException');
 
 class UserRepositoryTest extends CDbTestCase {
 
@@ -28,6 +29,20 @@ class UserRepositoryTest extends CDbTestCase {
 	public function testGetNonExistingUser() {
 		$user = $this->repository->getUser(9001);
 		$this->assertTrue($user === null);
+	}
+
+	public function testSaveExistingUser() {
+		$this->setExpectedException('UserSaveException');
+		$userArr = $this->users['user1'];
+
+		$user = new User();
+		$user->attributes = $userArr;
+
+		$this->assertTrue($this->repository->saveUser($user) !== true);
+	}
+
+	public function testSaveNonExistingUser() {
+
 	}
 
 } 
